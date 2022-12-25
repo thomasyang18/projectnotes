0f430c2db7752550aae5e7149bd96c9d312778c5
thomasyang18 <tomyangtt18@gmail.com>
2022-12-25 08:16:08 -0500

    Upd

--- submit.sh
+++ submit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# First argument is project name. Second flag ('-n'), if enabled, forces creation of new project folder. 
+# Otherwise, script exits if project folder doesn't exist.
+
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
@@ -7,7 +10,15 @@ if [ $# -eq 0 ]
 fi
 
 cur_date=$(date +'%m-%d-%Y')
+
+if ! [[ $2 == "-n" || -e projects/$1 ]]
+  then
+    echo "projects/$1 does not exist; try re-running with flag -n"
+    exit
+fi
+
 mkdir -p projects/$1
+    
 
 gen_file="projects/$1/$cur_date.html"
 
